405e2da9212d3bc2711404f37185237ee09f13bf
Reasno <guxi99@gmail.com>
2021-01-26 18:43:23 +0800

    feat: add redis provider

---
+++ pkg/otredis/provider.go
@@ -0,0 +1,26 @@
+package otredis
+
+import (
+	"github.com/DoNewsCode/std/pkg/contract"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/go-redis/redis/v8"
+	"github.com/opentracing/opentracing-go"
+)
+
+func ProvideRedis(logging log.Logger, conf contract.ConfigAccessor, tracer opentracing.Tracer) (redis.UniversalClient, func()) {
+	client := redis.NewUniversalClient(
+		&redis.UniversalOptions{
+			Addrs:    conf.Strings("redis.addrs"),
+			DB:       conf.Int("redis.database"),
+			Password: conf.String("redis.password"),
+		})
+	client.AddHook(
+		NewHook(tracer, conf.Strings("redis.addrs"),
+			conf.Int("redis.database")))
+	return client, func() {
+		if err := client.Close(); err != nil {
+			level.Error(logging).Log("err", err.Error())
+		}
+	}
+}
