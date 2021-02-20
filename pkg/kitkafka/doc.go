9b40686139997ee9ce7dc4b2cfa7375e9781379c
Reasno <guxi99@gmail.com>
2021-02-20 13:48:12 +0800

    ci: fix kafka port

--- pkg/kitkafka/doc.go
+++ pkg/kitkafka/doc.go
@@ -4,7 +4,14 @@ Package kitkafka provides a kafka transport for go kit.
 Introduction
 
 Go kit has some great properties, such as allowing multiple transport to be used
-simultaneously. Sadly it limits itself to only support RPCs. In real project,
-messaging is an inevitable path we must go down.
+simultaneously. Sadly it limits itself to only support RPCs. In real projects
+with many decoupled component, messaging is an inevitable path we must go down.
+
+Go kit models the RPCs as:
+
+	func(context.Context, request interface{}) (response interface{}, err error)
+
+Package kitkafka treat messaging as a special case RPC, where the response is always ignored.
+By using the same model, package kafka brings all
 */
 package kitkafka
