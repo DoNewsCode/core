07708ea1f30fdb3ae83e9da7107fb4e343628557
Reasno <guxi99@gmail.com>
2021-02-20 23:27:06 +0800

    test: cover large request

--- pkg/clihttp/client_test.go
+++ pkg/clihttp/client_test.go
@@ -8,10 +8,6 @@ import (
 	"github.com/opentracing/opentracing-go/mocktracer"
 )
 
-type MockClient struct{}
-
-func (m MockClient) Do(request *http.Request) (*http.Response, error) { return &http.Response{}, nil }
-
 func TestClient_Do(t *testing.T) {
 	t.Run("normal", func(t *testing.T) {
 		t.Parallel()
@@ -27,8 +23,19 @@ func TestClient_Do(t *testing.T) {
 	t.Run("large request", func(t *testing.T) {
 		t.Parallel()
 		tracer := mocktracer.New()
-		client := NewClient(tracer)
-		req, _ := http.NewRequest("POST", "https://baidu.com", strings.NewReader(strings.Repeat("f", 10000000)))
+		client := NewClient(tracer, WithRequestLogThreshold(1))
+		req, _ := http.NewRequest("POST", "https://baidu.com", strings.NewReader(strings.Repeat("f", 10)))
+		_, _ = client.Do(req)
+		if len(tracer.FinishedSpans()) == 0 {
+			t.Fatalf("finished span want at least  1, got %d", len(tracer.FinishedSpans()))
+		}
+	})
+
+	t.Run("large response", func(t *testing.T) {
+		t.Parallel()
+		tracer := mocktracer.New()
+		client := NewClient(tracer, WithResponseLogThreshold(1))
+		req, _ := http.NewRequest("GET", "https://baidu.com", nil)
 		_, _ = client.Do(req)
 		if len(tracer.FinishedSpans()) == 0 {
 			t.Fatalf("finished span want at least  1, got %d", len(tracer.FinishedSpans()))
