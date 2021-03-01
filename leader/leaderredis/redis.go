387916c05a8077ce96cc72a6acd35d230d6a6824
Reasno <guxi99@gmail.com>
2021-03-01 15:58:31 +0800

    test: fix redis driver test

--- leader/leaderredis/redis.go
+++ leader/leaderredis/redis.go
@@ -8,39 +8,103 @@ import (
 
 	"github.com/DoNewsCode/core/contract"
 	"github.com/go-redis/redis/v8"
-	"github.com/pkg/errors"
 )
 
 // RedisDriver is a simple redis leader election implementation.
 type RedisDriver struct {
-	client redis.UniversalClient
-	keyer  contract.Keyer
+	client       redis.UniversalClient
+	keyer        contract.Keyer
+	expiration   time.Duration
+	pollInterval time.Duration
+	cancel       func()
+	sha          string
+}
+
+// Option is type of the options to config *RedisDriver
+type Option func(driver *RedisDriver)
+
+// WithExpiration is an option that configures the expiration of redis key. A new
+// round of leader election will start after the key expires.
+func WithExpiration(duration time.Duration) Option {
+	return func(driver *RedisDriver) {
+		driver.expiration = duration
+	}
+}
+
+// WithPollInterval is an option that configures the poll interval of followers.
+// The followers will periodically try to overthrown the leader, but only succeed
+// when the leader key is missing in redis.
+func WithPollInterval(duration time.Duration) Option {
+	return func(driver *RedisDriver) {
+		driver.pollInterval = duration
+	}
+}
+
+// NewRedisDriver creates the newly created *RedisDriver with the given configuration.
+func NewRedisDriver(client redis.UniversalClient, keyer contract.Keyer, opts ...Option) *RedisDriver {
+	driver := &RedisDriver{
+		client:       client,
+		keyer:        keyer,
+		expiration:   time.Minute,
+		pollInterval: time.Second,
+		sha:          "",
+	}
+	for _, f := range opts {
+		f(driver)
+	}
+	return driver
 }
 
 // Campaign starts the leader election using redis. It will bock until this node becomes leader or the context is expired.
 func (r *RedisDriver) Campaign(ctx context.Context) error {
 	for {
 		hostname, _ := os.Hostname()
-		ok, err := r.client.SetNX(ctx, r.keyer.Key(":", "leader"), hostname, time.Minute).Result()
+		ok, err := r.client.SetNX(ctx, r.keyer.Key(":", "leader"), hostname, r.expiration).Result()
 		if err != redis.Nil && err != nil {
 			return fmt.Errorf("error when running compaign: %w", err)
 		}
 		if ok {
+			var ctx context.Context
+			ctx, r.cancel = context.WithCancel(context.Background())
+			go func() {
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					case <-time.After(3 * r.expiration / 4):
+						r.client.Expire(ctx, r.keyer.Key(":", "leader"), r.expiration).Result()
+					}
+				}
+			}()
 			return nil
 		}
-		time.Sleep(time.Second)
+		time.Sleep(r.pollInterval)
 	}
 }
 
 // Resign gives up the leadership using redis. If the current node is not a leader, this is an no op.
-func (r RedisDriver) Resign(ctx context.Context) error {
-	hostname, _ := os.Hostname()
-	// TODO: make read and delete atomic
-	leader, _ := r.client.Get(ctx, r.keyer.Key(":", "leader")).Result()
-	if hostname == leader {
-		r.client.Del(ctx, r.keyer.Key(":", "leader")).Result()
-		return nil
+func (r *RedisDriver) Resign(ctx context.Context) error {
+	if r.cancel != nil {
+		r.cancel()
 	}
-	return errors.New("not leader")
+	hostname, _ := os.Hostname()
+	if r.sha == "" {
+		var err error
+		r.sha, err = r.client.ScriptLoad(context.Background(), `
+if redis.call("get",KEYS[1]) == ARGV[1] then
+    return redis.call("del",KEYS[1])
+else
+    return 0
+end
+`).Result()
+		if err != nil {
+			return fmt.Errorf("unable to resign: %w", err)
+		}
+	}
+	_, err := r.client.EvalSha(ctx, r.sha, []string{r.keyer.Key(":", "leader")}, hostname).Result()
+	if err != nil {
+		return fmt.Errorf("unable to resign: %w", err)
+	}
+	return nil
 
 }
--- leader/leaderredis/redis_test.go
+++ leader/leaderredis/redis_test.go
@@ -5,13 +5,17 @@ package leaderredis
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/DoNewsCode/core/events"
 	"github.com/DoNewsCode/core/key"
+	"github.com/DoNewsCode/core/leader"
 	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCampaign(t *testing.T) {
+	t.Parallel()
 	client := redis.NewUniversalClient(&redis.UniversalOptions{})
 	client.FlushAll(context.Background()).Result()
 
@@ -28,4 +32,40 @@ func TestCampaign(t *testing.T) {
 
 	_, err = client.Get(context.Background(), "leader").Result()
 	assert.Error(t, err)
+}
+
+func TestNewRedisDriver(t *testing.T) {
+	t.Parallel()
+	client := redis.NewUniversalClient(&redis.UniversalOptions{})
+	client.FlushAll(context.Background()).Result()
+
+	driver := NewRedisDriver(client, key.New("foo", "bar"), WithExpiration(time.Hour), WithPollInterval(time.Minute))
+	assert.NotNil(t, driver)
+}
+
+func TestElection(t *testing.T) {
+	t.Parallel()
+	var dispatcher = &events.SyncDispatcher{}
+	var e1, e2 *leader.Election
+	var driver = NewRedisDriver(redis.NewUniversalClient(&redis.UniversalOptions{}), key.New("testElection"), WithPollInterval(time.Microsecond), WithExpiration(time.Second))
+
+	e1 = leader.NewElection(dispatcher, driver)
+	e2 = leader.NewElection(dispatcher, driver)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	e1.Campaign(ctx)
+	assert.Equal(t, e1.Status().IsLeader(), true)
+
+	go e2.Campaign(ctx)
+	<-time.After(3 * time.Second)
+
+	assert.Equal(t, true, e1.Status().IsLeader())
+	assert.Equal(t, false, e2.Status().IsLeader())
+
+	e1.Resign(ctx)
+	time.Sleep(10 * time.Millisecond)
+	assert.Equal(t, false, e1.Status().IsLeader())
+	assert.Equal(t, true, e2.Status().IsLeader())
+	e2.Resign(ctx)
+	cancel()
 }
