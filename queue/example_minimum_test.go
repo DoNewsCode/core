af36ddc17304d2ad165462a3a652a36e70185eca
reasno <guxi99@gmail.com>
2021-03-03 00:20:08 +0800

    fix(test):data race in example_minimum_test.go

--- queue/example_minimum_test.go
+++ queue/example_minimum_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/DoNewsCode/core/contract"
 	"github.com/DoNewsCode/core/events"
 	"github.com/DoNewsCode/core/queue"
-	"sync"
 	"time"
 )
 
@@ -14,18 +13,20 @@ func Example_minimum() {
 	dispatcher := events.SyncDispatcher{}
 	queueDispatcher := queue.WithQueue(&dispatcher, queue.NewInProcessDriver())
 	ctx, cancel := context.WithCancel(context.Background())
-	queueDispatcher.Dispatch(ctx, queue.Persist(events.Of(1), queue.Defer(time.Second)))
-	queueDispatcher.Dispatch(ctx, queue.Persist(events.Of(2), queue.Defer(time.Hour)))
-	var wg sync.WaitGroup
-	wg.Add(1)
+	defer cancel()
+
+	var ch = make(chan struct{})
 	go queueDispatcher.Consume(ctx)
+
 	queueDispatcher.Subscribe(events.Listen(events.From(1), func(ctx context.Context, event contract.Event) error {
 		fmt.Println(event.Data())
-		wg.Done()
+		ch <- struct{}{}
 		return nil
 	}))
-	wg.Wait()
-	cancel()
+	queueDispatcher.Dispatch(ctx, queue.Persist(events.Of(1), queue.Defer(time.Second)))
+	queueDispatcher.Dispatch(ctx, queue.Persist(events.Of(2), queue.Defer(time.Hour)))
+
+	<-ch
 
 	// Output:
 	// 1
