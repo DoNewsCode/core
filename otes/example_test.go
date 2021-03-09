10b90355f55dff063fc68748fc8f8b45e58074a2
另维64 <1142674342@qq.com>
2021-03-09 23:31:15 +0800

    feat(es): Add otes package (#61)
    * feat: add package otelastic
    
    * doc: fix spelling mistake
    
    * fix: fix es review
    
    * ci: add elasticsearch
    
    * Update go.yml
    
    * build: add integration build tag
    
    * fix: fix esAdapterLog
    
    * fix: adjust esConfig and del log
    
    * fix: fix panic
    
    * feat: replace es log
    
    * test: fix test fail
    
    Co-authored-by: “marvin” <“mawencheng@infinities.com.cn”>
    Co-authored-by: 谷溪 <guxi99@gmail.com>

---
+++ otes/example_test.go
@@ -0,0 +1,22 @@
+// +build integration
+
+package otes_test
+
+import (
+	"fmt"
+	"github.com/DoNewsCode/core"
+	"github.com/DoNewsCode/core/otes"
+	"github.com/olivere/elastic/v7"
+)
+
+func Example() {
+	c := core.New(core.WithInline("log.level", "none"))
+	c.ProvideEssentials()
+	c.Provide(otes.Providers())
+	c.Invoke(func(esClient *elastic.Client) {
+		running := esClient.IsRunning()
+		fmt.Println(running)
+	})
+	// Output:
+	// true
+}
