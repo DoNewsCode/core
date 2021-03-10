31df48d6a7af0e1fbb2e7756df3ad19b139373dd
“marvin” <mawencheng@infinities.com.cn>
2021-03-10 22:41:07 +0800

    test: add etcd example test

---
+++ otetcd/exmaple_test.go
@@ -0,0 +1,30 @@
+// +build integration
+
+package otetcd_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/DoNewsCode/core"
+	"github.com/DoNewsCode/core/otetcd"
+	"go.etcd.io/etcd/clientv3"
+	"log"
+)
+
+func Example() {
+	c := core.New()
+	c.ProvideEssentials()
+	c.Provide(otetcd.Providers())
+	c.Invoke(func(cli *clientv3.Client) {
+		_, err := cli.Put(context.TODO(), "foo", "bar")
+		if err != nil {
+			log.Fatal("etcd put failed")
+		}
+		resp, _ := cli.Get(context.TODO(), "foo")
+		for _, ev := range resp.Kvs {
+			fmt.Printf("%s : %s\n", ev.Key, ev.Value)
+		}
+	})
+	// Output:
+	// foo : bar
+}
