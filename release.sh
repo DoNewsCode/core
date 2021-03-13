57ef5ec5c139f1e08decf484a12bf21a1b5c4402
reasno <guxi99@gmail.com>
2021-03-13 13:35:25 +0800

    chore: modify release script

--- release.sh
+++ release.sh
@@ -1,2 +1,4 @@
-git-chglog --output CHANGELOG.md v0.2.0..
+git-chglog --next-tag $1 --output CHANGELOG.md v0.2.0..
 git commit CHANGELOG.md -m "chore: generate changelog"
+git tag $1
+git push && git push --tags
