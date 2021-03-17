4c30692c945b34f7ff60a46ec8f2dfc9689b7231
Trock <g_trock@163.com>
2021-03-17 10:18:25 +0800

    feat: add CronLogAdapter

---
+++ otcron/log.go
@@ -0,0 +1,21 @@
+package otcron
+
+import (
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+)
+
+// CronLogAdapter is an adapter between kitlog and cron logger interface
+type CronLogAdapter struct {
+	Logging log.Logger
+}
+
+// Info implements cron.Logger
+func (r CronLogAdapter) Info(msg string, keysAndValues ...interface{}) {
+	_ = level.Info(r.Logging).Log("msg", msg, keysAndValues)
+}
+
+// Error implements cron.Logger
+func (r CronLogAdapter) Error(err error, msg string, keysAndValues ...interface{}) {
+	_ = level.Error(r.Logging).Log("msg", msg, "error", err, keysAndValues)
+}
