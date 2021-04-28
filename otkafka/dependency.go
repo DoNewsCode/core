9280ba72d37d2355baa92083fa01909c60c1091c
Trock <g_trock@163.com>
2021-04-28 15:38:06 +0800

    feat: Add kafka metrics

--- otkafka/dependency.go
+++ otkafka/dependency.go
@@ -2,6 +2,7 @@ package otkafka
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/DoNewsCode/core/config"
 	"github.com/DoNewsCode/core/contract"
@@ -48,18 +49,22 @@ type in struct {
 	Tracer            opentracing.Tracer `optional:"true"`
 	Conf              contract.ConfigAccessor
 	Logger            log.Logger
+	ReaderStats       *ReaderStats `optional:"true"`
+	WriterStats       *WriterStats `optional:"true"`
 }
 
 // out is the result of provideKafkaFactory.
 type out struct {
 	di.Out
 
-	ReaderFactory ReaderFactory
-	WriterFactory WriterFactory
-	ReaderMaker   ReaderMaker
-	WriterMaker   WriterMaker
-	Reader        *kafka.Reader
-	Writer        *kafka.Writer
+	ReaderFactory   ReaderFactory
+	WriterFactory   WriterFactory
+	ReaderMaker     ReaderMaker
+	WriterMaker     WriterMaker
+	Reader          *kafka.Reader
+	Writer          *kafka.Writer
+	ReaderCollector *readerCollector
+	WriterCollector *writerCollector
 }
 
 // provideKafkaFactory creates the ReaderFactory and WriterFactory. It is
@@ -78,13 +83,28 @@ func provideKafkaFactory(p in) (out, func(), func(), error) {
 	if err2 != nil {
 		level.Warn(p.Logger).Log("err", err2)
 	}
+	var readerCollector *readerCollector
+	var writerCollector *writerCollector
+	if p.ReaderStats != nil || p.WriterStats != nil {
+		var interval time.Duration
+		p.Conf.Unmarshal("kafkaMetrics.interval", &interval)
+		if p.ReaderStats != nil {
+			readerCollector = newReaderCollector(rf, p.ReaderStats, interval)
+		}
+		if p.WriterStats != nil {
+			writerCollector = newWriterCollector(wf, p.WriterStats, interval)
+		}
+	}
+
 	return out{
-		ReaderMaker:   rf,
-		ReaderFactory: rf,
-		WriterMaker:   wf,
-		WriterFactory: wf,
-		Reader:        dr,
-		Writer:        dw,
+		ReaderMaker:     rf,
+		ReaderFactory:   rf,
+		WriterMaker:     wf,
+		WriterFactory:   wf,
+		Reader:          dr,
+		Writer:          dw,
+		ReaderCollector: readerCollector,
+		WriterCollector: writerCollector,
 	}, wc, rc, nil
 }
 
@@ -160,6 +180,10 @@ func provideWriterFactory(p in) (WriterFactory, func()) {
 	return WriterFactory{factory}, factory.Close
 }
 
+type metricsConf struct {
+	Interval config.Duration `json:"interval" yaml:"interval"`
+}
+
 type configOut struct {
 	di.Out
 
@@ -172,12 +196,19 @@ func provideConfig() configOut {
 			Owner: "kitkafka",
 			Data: map[string]interface{}{
 				"kafka": map[string]interface{}{
-					"reader": ReaderConfig{
-						Brokers: []string{"127.0.0.1:9092"},
+					"reader": map[string]interface{}{
+						"default": ReaderConfig{
+							Brokers: []string{"127.0.0.1:9092"},
+						},
 					},
-					"writer": WriterConfig{
-						Brokers: []string{"127.0.0.1:9092"},
+					"writer": map[string]interface{}{
+						"default": WriterConfig{
+							Brokers: []string{"127.0.0.1:9092"},
+						},
 					},
+				},
+				"kafkaMetrics": metricsConf{
+					Interval: config.Duration{Duration: 15 * time.Second},
 				},
 			},
 			Comment: "",
---
+++ otkafka/writer_metrics.go
@@ -0,0 +1,95 @@
+package otkafka
+
+import (
+	"github.com/go-kit/kit/metrics"
+	"github.com/segmentio/kafka-go"
+	"time"
+)
+
+type writerCollector struct {
+	factory  WriterFactory
+	stats    *WriterStats
+	interval time.Duration
+}
+
+// WriterStats is a collection of metrics for kafka writer info.
+type WriterStats struct {
+	Writes   metrics.Counter
+	Messages metrics.Counter
+	Bytes    metrics.Counter
+	Errors   metrics.Counter
+
+	MaxAttempts  metrics.Gauge
+	MaxBatchSize metrics.Gauge
+	BatchTimeout metrics.Gauge
+	ReadTimeout  metrics.Gauge
+	WriteTimeout metrics.Gauge
+	RequiredAcks metrics.Gauge
+	Async        metrics.Gauge
+
+	BatchTime  ThreeStats
+	WriteTime  ThreeStats
+	WaitTime   ThreeStats
+	Retries    ThreeStats
+	BatchSize  ThreeStats
+	BatchBytes ThreeStats
+}
+
+// newCollector creates a new kafka writer wrapper containing the name of the reader.
+func newWriterCollector(factory WriterFactory, stats *WriterStats, interval time.Duration) *writerCollector {
+	return &writerCollector{
+		factory:  factory,
+		stats:    stats,
+		interval: interval,
+	}
+}
+
+// collectConnectionStats collects kafka writer info for Prometheus to scrape.
+func (d *writerCollector) collectConnectionStats() {
+	for k, v := range d.factory.List() {
+		writer := v.Conn.(*kafka.Writer)
+		stats := writer.Stats()
+		withValues := []string{"writer", k, "topic", stats.Topic}
+
+		d.stats.Writes.With(withValues...).Add(float64(stats.Writes))
+		d.stats.Messages.With(withValues...).Add(float64(stats.Messages))
+		d.stats.Bytes.With(withValues...).Add(float64(stats.Bytes))
+		d.stats.Errors.With(withValues...).Add(float64(stats.Errors))
+
+		d.stats.BatchTime.Min.With(withValues...).Add(stats.BatchTime.Min.Seconds())
+		d.stats.BatchTime.Max.With(withValues...).Add(stats.BatchTime.Max.Seconds())
+		d.stats.BatchTime.Avg.With(withValues...).Add(stats.BatchTime.Avg.Seconds())
+
+		d.stats.WriteTime.Min.With(withValues...).Add(stats.WriteTime.Min.Seconds())
+		d.stats.WriteTime.Max.With(withValues...).Add(stats.WriteTime.Max.Seconds())
+		d.stats.WriteTime.Avg.With(withValues...).Add(stats.WriteTime.Avg.Seconds())
+
+		d.stats.WaitTime.Min.With(withValues...).Add(stats.WaitTime.Min.Seconds())
+		d.stats.WaitTime.Max.With(withValues...).Add(stats.WaitTime.Max.Seconds())
+		d.stats.WaitTime.Avg.With(withValues...).Add(stats.WaitTime.Avg.Seconds())
+
+		d.stats.Retries.Min.With(withValues...).Add(float64(stats.Retries.Min))
+		d.stats.Retries.Max.With(withValues...).Add(float64(stats.Retries.Max))
+		d.stats.Retries.Avg.With(withValues...).Add(float64(stats.Retries.Avg))
+
+		d.stats.BatchSize.Min.With(withValues...).Add(float64(stats.BatchSize.Min))
+		d.stats.BatchSize.Max.With(withValues...).Add(float64(stats.BatchSize.Max))
+		d.stats.BatchSize.Avg.With(withValues...).Add(float64(stats.BatchSize.Avg))
+
+		d.stats.BatchBytes.Min.With(withValues...).Add(float64(stats.BatchBytes.Min))
+		d.stats.BatchBytes.Max.With(withValues...).Add(float64(stats.BatchBytes.Max))
+		d.stats.BatchBytes.Avg.With(withValues...).Add(float64(stats.BatchBytes.Avg))
+
+		d.stats.MaxAttempts.With(withValues...).Set(float64(stats.MaxAttempts))
+		d.stats.MaxBatchSize.With(withValues...).Set(float64(stats.MaxBatchSize))
+		d.stats.BatchTimeout.With(withValues...).Set(stats.BatchTimeout.Seconds())
+		d.stats.ReadTimeout.With(withValues...).Set(stats.ReadTimeout.Seconds())
+		d.stats.WriteTimeout.With(withValues...).Set(stats.WriteTimeout.Seconds())
+		d.stats.RequiredAcks.With(withValues...).Set(float64(stats.RequiredAcks))
+		var async float64
+		if stats.Async {
+			async = 1.0
+		}
+		d.stats.Async.With(withValues...).Set(async)
+	}
+}
