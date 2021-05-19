6bae6a2a95edbd1d7692c05c0471f6377a749426
Trock <35254251+GGXXLL@users.noreply.github.com>
2021-05-19 18:36:06 +0800

    feat: Add kafka metrics (#130)
    * feat: Add kafka metrics
    
    * test: add some test
    
    * feat: Add kafka metrics
    
    * test: add some test
    
    * test: add some test
    
    * test: add some test
    
    * fix: update .gitignore
    
    * fix: missing .env file
    
    * test: fix test error
    
    Co-authored-by: 谷溪 <guxi99@gmail.com>

--- observability/metrics.go
+++ observability/metrics.go
@@ -1,6 +1,7 @@
 package observability
 
 import (
+	"github.com/DoNewsCode/core/otkafka"
 	"sync"
 
 	"github.com/DoNewsCode/core/contract"
@@ -98,5 +99,392 @@ func ProvideRedisMetrics(appName contract.AppName, env contract.Env) *otredis.Ga
 			Name:      "redis_stale_connections",
 			Help:      "number of stale connections removed from the pool",
 		}, []string{"dbname"}),
+	}
+}
+
+// ProvideKafkaReaderMetrics returns a *otkafka.ReaderStats that measures the reader info in kafka.
+// It is meant to be consumed by the otkafka.Providers.
+func ProvideKafkaReaderMetrics(appName contract.AppName, env contract.Env) *otkafka.ReaderStats {
+	labels := []string{"reader", "client_id", "topic", "partition"}
+
+	return &otkafka.ReaderStats{
+		Dials: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_dial_count",
+			Help:      "",
+		}, labels),
+		Fetches: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_fetch_count",
+			Help:      "",
+		}, labels),
+		Messages: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_message_count",
+			Help:      "",
+		}, labels),
+		Bytes: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_message_bytes",
+			Help:      "",
+		}, labels),
+		Rebalances: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_rebalance_count",
+			Help:      "",
+		}, labels),
+		Timeouts: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_timeout_count",
+			Help:      "",
+		}, labels),
+		Errors: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_error_count",
+			Help:      "",
+		}, labels),
+		Offset: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_offset",
+			Help:      "",
+		}, labels),
+		Lag: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_lag",
+			Help:      "",
+		}, labels),
+		MinBytes: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_bytes_min",
+			Help:      "",
+		}, labels),
+		MaxBytes: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_bytes_max",
+			Help:      "",
+		}, labels),
+		MaxWait: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_fetch_wait_max",
+			Help:      "",
+		}, labels),
+		QueueLength: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_queue_length",
+			Help:      "",
+		}, labels),
+		QueueCapacity: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_reader_queue_capacity",
+			Help:      "",
+		}, labels),
+		DialTime: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_dial_seconds_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_dial_seconds_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_dial_seconds_avg",
+				Help:      "",
+			}, labels),
+		},
+		ReadTime: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_read_seconds_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_read_seconds_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_read_seconds_avg",
+				Help:      "",
+			}, labels),
+		},
+		WaitTime: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_wait_seconds_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_wait_seconds_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_wait_seconds_avg",
+				Help:      "",
+			}, labels),
+		},
+		FetchSize: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_fetch_size_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_fetch_size_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_fetch_size_avg",
+				Help:      "",
+			}, labels),
+		},
+		FetchBytes: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_fetch_bytes_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_fetch_bytes_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_reader_fetch_bytes_avg",
+				Help:      "",
+			}, labels),
+		},
+	}
+}
+
+// ProvideKafkaWriterMetrics returns a *otkafka.WriterStats that measures the writer info in kafka.
+// It is meant to be consumed by the otkafka.Providers.
+func ProvideKafkaWriterMetrics(appName contract.AppName, env contract.Env) *otkafka.WriterStats {
+	labels := []string{"writer", "topic"}
+	return &otkafka.WriterStats{
+		Writes: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_write_count",
+			Help:      "",
+		}, labels),
+		Messages: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_message_count",
+			Help:      "",
+		}, labels),
+		Bytes: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_message_bytes",
+			Help:      "",
+		}, labels),
+		Errors: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_error_count",
+			Help:      "",
+		}, labels),
+		MaxAttempts: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_attempts_max",
+			Help:      "",
+		}, labels),
+		MaxBatchSize: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_batch_max",
+			Help:      "",
+		}, labels),
+		BatchTimeout: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_batch_timeout",
+			Help:      "",
+		}, labels),
+		ReadTimeout: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_read_timeout",
+			Help:      "",
+		}, labels),
+		WriteTimeout: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_write_timeout",
+			Help:      "",
+		}, labels),
+		RequiredAcks: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_acks_required",
+			Help:      "",
+		}, labels),
+		Async: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: appName.String(),
+			Subsystem: env.String(),
+			Name:      "kafka_writer_async",
+			Help:      "",
+		}, labels),
+		BatchTime: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_seconds_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_seconds_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_seconds_avg",
+				Help:      "",
+			}, labels),
+		},
+		WriteTime: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_write_seconds_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_write_seconds_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_write_seconds_avg",
+				Help:      "",
+			}, labels),
+		},
+		WaitTime: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_wait_seconds_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_wait_seconds_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_wait_seconds_avg",
+				Help:      "",
+			}, labels),
+		},
+		Retries: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_retries_count_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_retries_count_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_retries_count_avg",
+				Help:      "",
+			}, labels),
+		},
+		BatchSize: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_size_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_size_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_size_avg",
+				Help:      "",
+			}, labels),
+		},
+		BatchBytes: otkafka.ThreeStats{
+			Min: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_bytes_min",
+				Help:      "",
+			}, labels),
+			Max: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_bytes_max",
+				Help:      "",
+			}, labels),
+			Avg: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+				Namespace: appName.String(),
+				Subsystem: env.String(),
+				Name:      "kafka_writer_batch_bytes_avg",
+				Help:      "",
+			}, labels),
+		},
 	}
 }
