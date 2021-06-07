13402e6220115b8cc306573c3144222668dbb06a
Reasno <guxi99@gmail.com>
2021-06-07 15:59:58 +0800

    feat(config): support configuration ReloadedEvent

--- config/event.go
+++ config/event.go
@@ -4,14 +4,18 @@ import (
 	"github.com/DoNewsCode/core/contract"
 )
 
+// ReloadedEvent is an event that triggers the configuration reloads
 type ReloadedEvent struct {
+	// NewConf is the latest configuration after the reload.
 	NewConf contract.ConfigAccessor
 }
 
+// Type implements contract.Event
 func (r ReloadedEvent) Type() string {
 	return "core.internal.reloaded_event"
 }
 
+// Data implements contract.Event
 func (r ReloadedEvent) Data() interface{} {
 	return r.NewConf
 }
