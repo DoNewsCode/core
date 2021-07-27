68582a044d56f7c7db5bf78bb4fdca519e4d66be
Reasno <guxi99@gmail.com>
2021-07-27 10:49:14 +0800

    doc: add comment

--- leader/event.go
+++ leader/event.go
@@ -2,8 +2,10 @@ package leader
 
 type event string
 
+// OnStatusChanged is an event that triggers when the leadership has transited. It's payload is OnStatusChangedPayload.
 const OnStatusChanged event = "onStatusChanged"
 
+// OnStatusChangedPayload is the payload of OnStatusChanged.
 type OnStatusChangedPayload struct {
 	Status *Status
 }
