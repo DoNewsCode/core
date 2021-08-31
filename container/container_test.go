9d26ec7445e6d57a064da3fe9fc5a7fa979f2cae
Reasno <guxi99@gmail.com>
2021-08-31 17:03:23 +0800

    fix(container): test

--- container/container_test.go
+++ container/container_test.go
@@ -71,12 +71,18 @@ func TestContainer_AddModule(t *testing.T) {
 	}
 }
 
+type closer func()
+
+func (f closer) ProvideCloser() {
+	f()
+}
+
 func TestContainer_Shutdown(t *testing.T) {
 	var seq = 0
 	container := Container{}
-	container.AddModule(func() { assert.Equal(t, 2, seq); seq = 1 })
-	container.AddModule(func() { assert.Equal(t, 3, seq); seq = 2 })
-	container.AddModule(func() { assert.Equal(t, 0, seq); seq = 3 })
+	container.AddModule(closer(func() { assert.Equal(t, 2, seq); seq = 1 }))
+	container.AddModule(closer(func() { assert.Equal(t, 3, seq); seq = 2 }))
+	container.AddModule(closer(func() { assert.Equal(t, 0, seq); seq = 3 }))
 	container.Shutdown()
 	assert.Equal(t, 1, seq)
 }
