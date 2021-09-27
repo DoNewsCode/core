f90596118604deacf8d8feb961f3b409e913f636
谷溪 <guxi99@gmail.com>
2021-09-27 15:58:27 +0800

    feat: add WithoutCancel (#200)
    * feat: add WithoutCancel
    
    * fix: hound complaint
    
    * fix: WithoutCancel should panic when parent is nil
    
    * fix: tests
    
    * fix: naked return is not good for readability
    
    * chore: surpress linter
    
    Co-authored-by: Trock <g_trock@163.com>

---
+++ ctxmeta/without_cancel_test.go
@@ -0,0 +1,33 @@
+package ctxmeta
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithoutCancel(t *testing.T) {
+	key := struct{}{}
+	ctx := context.WithValue(context.Background(), key, "value")
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+
+	select {
+	case <-WithoutCancel(ctx).Done():
+		t.Fatal("context is cancelled")
+	default:
+	}
+
+	_, dead := WithoutCancel(ctx).Deadline()
+	assert.False(t, dead)
+	assert.Nil(t, WithoutCancel(ctx).Err())
+	assert.Equal(t, "value", WithoutCancel(ctx).Value(key))
+}
+
+func TestWithoutCancel_Nil(t *testing.T) {
+	defer func() {
+		assert.Equal(t, "cannot create context from nil parent", recover())
+	}()
+	WithoutCancel(nil) //nolint
+}
