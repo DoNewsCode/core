b8c91e750a78ca9951799ad978647a1f5a1975d8
reasno <guxi99@gmail.com>
2022-01-15 16:05:55 +0800

    fix: use time.Since

--- cron/options.go
+++ cron/options.go
@@ -55,7 +55,7 @@ func WithLogging(logger log.Logger) JobOptions {
 		innerRun := descriptor.Run
 		descriptor.Run = func(ctx context.Context) error {
 			due := GetCurrentSchedule(ctx)
-			delayed := time.Now().Sub(due)
+			delayed := time.Since(due)
 			logger = logging.WithContext(logger, ctx)
 			if delayed > time.Second {
 				log.With(logger, "delayed", delayed)
