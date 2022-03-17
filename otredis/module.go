2ca3cad7f6c0bf0487e0198d8c967b61330564e9
谷溪 <guxi99@gmail.com>
2022-03-17 16:00:19 +0800

    feat(otredis): add cleanup command to redis module (#239)
    * feat: add redis commands
    
    * feat: add redis commands
    
    * fix: cleanup code
    
    * chore: update minio env
    
    * fix: sync bug
    
    * fix: use waitGroup instead of errGroup for simplicity

---
+++ otredis/module.go
@@ -0,0 +1,41 @@
+package otredis
+
+import (
+	"github.com/DoNewsCode/core/di"
+	"github.com/go-kit/log"
+	"github.com/spf13/cobra"
+)
+
+// Module is the registration unit for package core. It provides redis command.
+type Module struct {
+	maker  Maker
+	logger log.Logger
+}
+
+// ModuleIn contains the input parameters needed for creating the new module.
+type ModuleIn struct {
+	di.In
+
+	Maker  Maker
+	Logger log.Logger
+}
+
+// New creates a Module.
+func New(in ModuleIn) Module {
+	return Module{
+		maker:  in.Maker,
+		logger: in.Logger,
+	}
+}
+
+// ProvideCommand provides redis commands.
+func (m Module) ProvideCommand(command *cobra.Command) {
+	cleanupCmd := NewCleanupCommand(m.maker, m.logger)
+	redisCmd := &cobra.Command{
+		Use:   "redis",
+		Short: "manage redis",
+		Long:  "manage redis, such as cleaning up redis cache",
+	}
+	redisCmd.AddCommand(cleanupCmd)
+	command.AddCommand(redisCmd)
+}
