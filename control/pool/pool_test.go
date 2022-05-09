273b661b0b9b6abf113cafd1579afd9eccfee63b
Trock <g_trock@163.com>
2022-05-09 19:33:08 +0800

    fix: pool.Run need start before call Go method when use async mode.

--- control/pool/pool_test.go
+++ control/pool/pool_test.go
@@ -12,43 +12,52 @@ func TestPool_Go(t *testing.T) {
 	t.Parallel()
 	ctx, cancel := context.WithCancel(context.Background())
 	p := NewPool(WithConcurrency(1))
-	go p.Go(context.Background(), func(asyncContext context.Context) {
+	go p.Run(ctx)
+	time.Sleep(time.Millisecond)
+	p.Go(context.Background(), func(asyncContext context.Context) {
 		cancel()
 	})
-	p.Run(ctx)
+
 }
 
 func TestPool_FallbackToSyncMode(t *testing.T) {
 	t.Parallel()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-
 	p := NewPool(WithConcurrency(1))
-	p.Run(ctx)
+	go p.Run(ctx)
+	time.Sleep(time.Millisecond)
 
+	ts := time.Now()
 	var executed = make(chan struct{})
-	go func() {
-		// saturate the pool
-		p.Go(ctx, func(asyncContext context.Context) {
-			time.Sleep(time.Second)
-		})
-		// fallback to sync mode
-		p.Go(ctx, func(asyncContext context.Context) {
-			close(executed)
-		})
-	}()
+
+	// saturate the pool
+	p.Go(ctx, func(asyncContext context.Context) {
+		time.Sleep(time.Second)
+	})
+	// fallback to sync mode
+	p.Go(ctx, func(asyncContext context.Context) {
+		close(executed)
+	})
 	<-executed
+	// job channel not be blocked, so the interval should be less than 1 second
+	if time.Since(ts) >= time.Second {
+		t.Fatal("timeout: sync mode should be used")
+	}
 }
 
 func TestPool_contextValue(t *testing.T) {
 	t.Parallel()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	p := NewPool(WithConcurrency(1))
+	go p.Run(ctx)
+	time.Sleep(time.Millisecond)
+
 	key := struct{}{}
 	requestContext := context.WithValue(context.Background(), key, "foo")
-	go p.Run(ctx)
+
 	p.Go(requestContext, func(asyncContext context.Context) {
 		if _, ok := asyncContext.Deadline(); ok {
 			t.Fatalf("asyncContext shouldn't have deadline set")
@@ -57,6 +66,7 @@ func TestPool_contextValue(t *testing.T) {
 		if value != "foo" {
 			t.Fatalf("want foo, got %s", value)
 		}
+		cancel()
 	})
 }
 
