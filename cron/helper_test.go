676c06a64bee2eaaaf07aa116d0ed642f8f63f6e
Trock <g_trock@163.com>
2022-07-20 14:51:08 +0800

    test(cron): reduce TestMockStartTimeFunc sleep duration

--- cron/helper_test.go
+++ cron/helper_test.go
@@ -19,7 +19,10 @@ func TestGetNextSchedule(t *testing.T) {
 func TestMockStartTimeFunc(t *testing.T) {
 	fakeNow, _ := time.ParseInLocation("2006-01-02 15:04:05", "2029-01-01 00:00:00", time.Local)
 	nowFunc := MockStartTimeFunc(fakeNow)
-	assert.True(t, nowFunc().Equal(fakeNow))
-	time.Sleep(time.Second)
-	assert.True(t, nowFunc().Second() == fakeNow.Add(time.Second).Second())
+	now1 := nowFunc()
+	now2 := time.Now()
+	
+	assert.True(t, now1.Equal(fakeNow))
+	time.Sleep(time.Millisecond)
+	assert.True(t, nowFunc().Sub(now1) == time.Since(now2))
 }
