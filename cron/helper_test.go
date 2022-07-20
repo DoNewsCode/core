8fe65a7a98f97a4a6bc986c6d36fe31558c5c8ad
Trock <g_trock@163.com>
2022-07-20 15:00:40 +0800

    fix(cron): TestMockStartTimeFunc flake

--- cron/helper_test.go
+++ cron/helper_test.go
@@ -19,10 +19,9 @@ func TestGetNextSchedule(t *testing.T) {
 func TestMockStartTimeFunc(t *testing.T) {
 	fakeNow, _ := time.ParseInLocation("2006-01-02 15:04:05", "2029-01-01 00:00:00", time.Local)
 	nowFunc := MockStartTimeFunc(fakeNow)
-	now1 := nowFunc()
-	now2 := time.Now()
-	
-	assert.True(t, now1.Equal(fakeNow))
+	now := nowFunc()
+
+	assert.True(t, now.Equal(fakeNow))
 	time.Sleep(time.Millisecond)
-	assert.True(t, nowFunc().Sub(now1) == time.Since(now2))
+	assert.True(t, nowFunc().After(now))
 }
